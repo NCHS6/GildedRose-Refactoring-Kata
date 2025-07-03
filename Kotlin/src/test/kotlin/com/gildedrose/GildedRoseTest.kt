0e46fb0d944412f3be181b27a2f844ff207bc23c
NCHS6 <nickleby.simmonds@gmail.com>
2025-07-03 14:55:19 +0100

    quality tests

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,18 +1,192 @@
 package com.gildedrose
 
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.justRun
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import kotlin.random.Random
+import kotlin.test.BeforeTest
 
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
-
+    fun `quality reduced by 1 for regular items`() {
+        val quality = Random.nextInt(1, 100)
+        val item = Item(name = "Blue Whale", sellIn = 10, quality = quality)
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertEquals(quality-1, gildedRose.items[0].quality)
     }
+
+    @Test
+    fun `quality reduced by 2 if passed sell by date`() {
+        val quality = Random.nextInt(1, 100)
+        val item = Item(name = "Blue Whale", sellIn = -2, quality = quality)
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertEquals(quality-2, gildedRose.items[0].quality)
+    }
+
+    @Test
+    fun `quality never becomes negative`() {
+        val item = Item(name = "Blue Whale", sellIn = -1, quality = 0)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertTrue(gildedRose.items[0].quality >= 0)
+    }
+
+    @Test
+    fun `quality increases by 1 per day for brie`() {
+        val quality = Random.nextInt(1, 100)
+        val item = Item(name = "Aged Brie", sellIn = 10, quality = 0)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertEquals(1, gildedRose.items[0].quality)
+    }
+
+    @Test
+    fun `quality increases by 2 per day for passed sell by date brie brie`() {
+        val quality = Random.nextInt(1, 100)
+        val item = Item(name = "Aged Brie", sellIn = -1, quality = 0)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertEquals(2, gildedRose.items[0].quality)
+    }
+
+    @Test
+    fun `quality never over 50 by passed sell by date brie`() {
+        val quality = Random.nextInt(1, 100)
+        val item = Item(name = "Aged Brie", sellIn = -1, quality = 49)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertTrue(gildedRose.items[0].quality <= 50)
+    }
+
+    @Test
+    fun `quality never over 50 by brie`() {
+        val quality = Random.nextInt(1, 100)
+        val item = Item(name = "Aged Brie", sellIn = -1, quality = 50)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertTrue(gildedRose.items[0].quality <= 50)
+    }
+
+    @Test
+    fun `quality never over 50 by regular items`() {
+        val item = Item(name = "Blue Moon", sellIn = 10, quality = 50)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertTrue(gildedRose.items[0].quality <= 50)
+    }
+
+    @Test
+    fun `quality never changes for Sulfuras`() {
+        val item = Item(name = "Sulfuras, Hand of Ragnaros", sellIn = 10, quality = 80)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertEquals(80, gildedRose.items[0].quality)
+    }
+
+    @Test
+    fun `quality never changes for passed sell by date Sulfuras`() {
+        val item = Item(name = "Sulfuras, Hand of Ragnaros", sellIn = -1, quality = 80)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertEquals(80, gildedRose.items[0].quality)
+    }
+
+    @Test
+    fun `quality increases by 1 with 11 or more sellIn for concert`() {
+        val item = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 11, quality = 20)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertEquals(21, gildedRose.items[0].quality)
+    }
+
+    @Test
+    fun `quality increases by 2 with 6-10 sellIn for concert`() {
+        val item = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 6, quality = 20)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertEquals(22, gildedRose.items[0].quality)
+    }
+
+    @Test
+    fun `quality increases by 5 with 1-5 sellIn for concert`() {
+        val item = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 1, quality = 20)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertEquals(23, gildedRose.items[0].quality)
+    }
+
+    @Test
+    fun `quality goes to 0 after the concert`() {
+        val item = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = -1, quality = 20)
+
+        val itemArr = listOf(item)
+        val gildedRose = GildedRose(itemArr)
+        gildedRose.updateQuality()
+        assertEquals(0, gildedRose.items[0].quality)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+//    @Test
+//    fun `regular items reduction in quality with random inputs`() {
+//        val item = mockk<Item>()
+//        every { item.name } returns "Blue Whale"
+//        every { item.sellIn } returns Random.nextInt(100)
+//        every { item.sellIn = any() } just Runs
+//        val quality = Random.nextInt(1, 100)
+//        every { item.quality } returns quality
+//        every { item.quality = any() } answers { item.quality - 1 }
+//
+//        val itemArr = listOf(item)
+//        val gildedRose = GildedRose(itemArr)
+//        gildedRose.updateQuality()
+//        assertEquals(quality, gildedRose.items[0].quality)
+//
+//        verify { gildedRose.items[0].quality }
+//    }
+
 
 }
 
